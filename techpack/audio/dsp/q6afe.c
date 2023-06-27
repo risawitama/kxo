e8ab6a1046bc81207e69c8d3f8f1087b98c6775a
Soumya Managoli <quic_c_smanag@quicinc.com>
2023-06-27 13:48:57 +0530

    techpack: audio: afe: Copyright update.
    Copyright update.
    
    Signed-off-by: Soumya Managoli <quic_c_smanag@quicinc.com>
    Change-Id: Ida14ef8f6a0779c2105ce3b25ab41c967fbab543
    (cherry picked from commit 462e91f669da0da90de8867a91972b4b0dbf9df4)
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Hadad <hadad@linuxmail.org>

--- techpack/audio/dsp/q6afe.c
+++ techpack/audio/dsp/q6afe.c
@@ -1,14 +1,7 @@
-/* Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+/* Copyright (c) 2012-2019, 2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: GPL-2.0-only
+*/
 #include <linux/slab.h>
 #include <linux/debugfs.h>
 #include <linux/kernel.h>
